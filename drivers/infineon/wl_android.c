fd825a53513d10510b37fb7984468e7e7497c438
Honglin Leng <dev@radxa.com>
2025-07-30 20:14:42 +0800

    drivers: infineon: Fix the conditional judgment in wl_android.c
    Link: https://github.com/radxa/rkwifibt/pull/3
    
    Signed-off-by: Honglin Leng <dev@radxa.com>
    Signed-off-by: Stephen Chen <stephen@radxa.com>

--- drivers/infineon/wl_android.c
+++ drivers/infineon/wl_android.c
@@ -8978,8 +8978,7 @@ wl_handle_private_cmd(struct net_device *net, char *command, u32 cmd_len)
 #endif /* DHD_BLOB_EXISTENCE_CHECK */
 		if ((rev_info_delim) &&
 			(strnicmp(rev_info_delim, CMD_COUNTRY_DELIMITER,
-			strlen(CMD_COUNTRY_DELIMITER)) == 0) &&
-			(rev_info_delim + 1)) {
+			strlen(CMD_COUNTRY_DELIMITER)) == 0)) {
 			revinfo  = bcm_atoi(rev_info_delim + 1);
 		}
 #ifdef SAVE_CONNECTION_WHEN_CC_UPDATE
